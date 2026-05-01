c65f7e4576dab7109233d54f136acceeb11d2e3f
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-01 23:50:36 +0545

    added: queryclientprovider in main.jsx

--- fetchingUsing_reactQuery/src/main.jsx
+++ fetchingUsing_reactQuery/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
+const queryclient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryclient}>
+
   <StrictMode>
     <App />
   </StrictMode>,
+
+  </QueryClientProvider>
 )
