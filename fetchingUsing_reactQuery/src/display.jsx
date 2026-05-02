ca53f4686057392da3a046e15dbe05a1ec84e7b2
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-02 23:32:38 +0545

    add: search feat

--- fetchingUsing_reactQuery/src/display.jsx
+++ fetchingUsing_reactQuery/src/display.jsx
@@ -1,32 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
-function Display()
-{
+function Display() {
+    const [search, setSearch] = useState("");
+    const { data, isLoading, error } = useQuery(
+        {
+            queryKey: ["post", search], // now the query key is different for every search so fetch data per search 
+            queryFn: async () => {
+                console.log("data fetch called");
 
-const {data, isLoading , error} = useQuery(
-    {
-        queryKey: ["post"],
-        queryFn: async ()=>{
-            const res =  await fetch("https://jsonplaceholder.typicode.com/posts");
-            return  res.json();
+                const res = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`);
+                return res.json();
+            },
+            // staleTime: 10000, // how long time does the data is fresh (default 0 sec so fetches data imidiately after we navigate somewhere else and come back )
+            enabled: !!search,
+
         }
-    }
-);
-    if(isLoading) return <p>data is loading please wait....</p>
-    if(error) return <p>error occurred</p>
+    );
+    if (isLoading) return <p>data is loading please wait....</p>
+    if (error) return <p>error occurred</p>
 
-    return(
-    <>
-        <h1>this is display component</h1>
-        <div>
+    if (!search) return (
+        <>
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
 
-            {
-                data.map((post)=>(
-                    <p key={post.id}>{post.title} </p>
-                ))
-            }
-        </div>
-    </>
+            <p>try to search something</p>
+        </>
+    )
+
+    return (
+        <>
+            <h1>this is display component</h1>
+
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+
+            <div>
+
+                {
+                    data.map((post) => (
+                        <p key={post.id}>{post.title} </p>
+                    ))
+                }
+            </div>
+        </>
 
     )
 }
