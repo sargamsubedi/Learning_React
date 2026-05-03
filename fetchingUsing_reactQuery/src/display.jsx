31804cd8d04ad8a1c229d8280dce81d1a366502a
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-03 22:57:14 +0545

    add: basic pagination

--- fetchingUsing_reactQuery/src/display.jsx
+++ fetchingUsing_reactQuery/src/display.jsx
@@ -1,44 +1,74 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Display() {
     const [search, setSearch] = useState("");
+    const [page, setPage] = useState(1);
+    const [allPosts, setAllPosts] = useState([]);
+
+
+
     const { data, isLoading, error } = useQuery(
         {
-            queryKey: ["post", search], // now the query key is different for every search so fetch data per search 
+            queryKey: ["posts", search, page], // now the query key is different for every search so fetch data per search 
             queryFn: async () => {
                 console.log("data fetch called");
 
-                const res = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`);
+                let url = search ? `https://jsonplaceholder.typicode.com/posts?q=${search}&_limit=5&_page=${page}` : `https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`
+
+                const res = await fetch(url);
                 return res.json();
             },
             // staleTime: 10000, // how long time does the data is fresh (default 0 sec so fetches data imidiately after we navigate somewhere else and come back )
-            enabled: !!search,
 
+            // enabled: !!search, // when to fetch data? if enable: true then only it fetches data
+            keepPreviousData: true
         }
     );
-    if (isLoading) return <p>data is loading please wait....</p>
+
+
+    useEffect(() => {
+        setPage(1);
+    }, [search])
+
+
+    useEffect(() => {
+        if (data) {
+            if (page === 1) {
+                setAllPosts(data)
+            }
+            else {
+
+                setAllPosts(prev => [...prev, ...data]);
+            }
+
+        }
+    }, [data,page])
+
+
+
+    if (isLoading && page===1) return <p>data is loading please wait....</p>
     if (error) return <p>error occurred</p>
 
-    if (!search) return (
-        <>
-            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
 
-            <p>try to search something</p>
-        </>
-    )
 
     return (
         <>
             <h1>this is display component</h1>
 
+
             <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+            <button onClick={() => setPage(prev => prev + 1)}>Load More</button>
+            {
+                isLoading && <p>Loading...</p>
+            }
+
 
             <div>
 
                 {
-                    data.map((post) => (
-                        <p key={post.id}>{post.title} </p>
+                    allPosts.map((post) => (
+                        <p key={post.id}>{post.id}: {post.title} </p>
                     ))
                 }
             </div>
