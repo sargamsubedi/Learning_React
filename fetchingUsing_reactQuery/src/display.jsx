511c326718c692fae4cdb0edbb8e8c7b54822c84
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-04 17:52:03 +0545

    add feat: changed manual infiniteScroll to automatic using useinfinitequery and commented out manual logic for future reference

--- fetchingUsing_reactQuery/src/display.jsx
+++ fetchingUsing_reactQuery/src/display.jsx
@@ -1,53 +1,91 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { useEffect, useRef, useState } from "react";
 import useDebounce from "./hooks/useDebounce";
 
 function Display() {
     const [search, setSearch] = useState("");
     const [page, setPage] = useState(1);
-    const [allPosts, setAllPosts] = useState([]);
+    // const [allPosts, setAllPosts] = useState([]); // we needed this when we implement infinite scroll manually , not when we use infinitequery. 
     const bottomRef = useRef(null);
 
     const debouncedSearch = useDebounce(search);
 
+    // fetching logic using usequery
 
-    const { data, isLoading, error } = useQuery(
-        {
-            queryKey: ["posts", debouncedSearch, page], // now the query key is different for every search so fetch data per search 
-            queryFn: async () => {
-                console.log("data fetch called");
+    // const { data, isLoading, error } = useQuery(
+    //     {
+    //         queryKey: ["posts", debouncedSearch, page], // now the query key is different for every search so fetch data per search 
+    //         queryFn: async () => {
+    //             console.log("data fetch called");
 
-                let url = search ? `https://jsonplaceholder.typicode.com/posts?q=${search}&_limit=10&_page=${page}` : `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
+    //             let url = search ? `https://jsonplaceholder.typicode.com/posts?q=${search}&_limit=10&_page=${page}` : `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
 
-                const res = await fetch(url);
-                return res.json();
-            },
-            // staleTime: 10000, // how long time does the data is fresh (default 0 sec so fetches data imidiately after we navigate somewhere else and come back )
+    //             const res = await fetch(url);
+    //             return res.json();
+    //         },
+    //         // staleTime: 10000, // how long time does the data is fresh (default 0 sec so fetches data imidiately after we navigate somewhere else and come back )
 
-            // enabled: !!search, // when to fetch data? if enable: true then only it fetches data
-            keepPreviousData: true
+    //         // enabled: !!search, // when to fetch data? if enable: true then only it fetches data
+    //         keepPreviousData: true
+    //     }
+    // );
+
+
+    // fetching logic using useInfiniteQuery
+
+    // structure of data
+    //     data = {
+    //   pages: [...], // it includes data fetched for each page pages = [ [page1 data],[page2 data],[page3 data] ]
+    //   pageParams: [...]  // This stores WHICH page was used each time , pageParams = [1, 2, 3]
+    // }
+
+    const { data, isLoading, error, fetchNextPage, hasNextPage } = useInfiniteQuery({
+        queryKey: ["posts", debouncedSearch, page],
+        queryFn: async ({ pageParam = 1 }) => {
+            console.log("fetching data for page: ", { pageParam });
+
+            let url = search ? `https://jsonplaceholder.typicode.com/posts?q=${search}&_limit=10&_page=${pageParam}` : `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageParam}`
+
+            const res = await fetch(url);
+            return res.json();
+        },
+        getNextPageParam: (lastPage, allPage) => {
+
+
+            // lastPage is data from latest/recent page
+
+            // structure of all page is: it contains data of all pages fetched till now 
+            //  allPages = [
+            //   [a,b,c,d,e],   // page 1
+            //   [f,g,h,i,j]    // page 2
+            // ]
+
+            if (lastPage.length == 0) return undefined; // if no data is fetched from last fetch dont fetch anymore
+            return (allPage.length + 1); // give lastpagefetched+1 for fetch new data 
+
         }
-    );
+
+    })
 
 
-    useEffect(() => {
-        setPage(1);
-    }, [debouncedSearch])
+    // useEffect(() => {
+    //     setPage(1);
+    // }, [debouncedSearch])
 
 
-    useEffect(() => {
-        if (data) {
-            if (page === 1) {
-                setAllPosts(data)
-            }
-            else {
+    // useEffect(() => {
+    //     if (data) {
+    //         if (page === 1) {
+    //             setAllPosts(data)
+    //         }
+    //         else {
 
-                setAllPosts(prev => [...prev, ...data]);
-            }
+    //             setAllPosts(prev => [...prev, ...data]);
+    //         }
 
-        }
+    //     }
 
-    }, [data, page])
+    // }, [data, page])
 
     // effect for infinite scroll.. 
     useEffect(() => {
@@ -55,9 +93,16 @@ function Display() {
         const observer = new IntersectionObserver((entries) => {
 
             if (entries[0].isIntersecting) {
-                if (!isLoading && data.length) {
-                    setPage(prev => prev + 1);
 
+                // used when manually implemented infiniteScroll
+
+                // if (!isLoading && data.length) {
+                //     // setPage(prev => prev + 1); 
+                // }
+
+                if(hasNextPage)
+                {
+                    fetchNextPage();
                 }
             }
 
@@ -74,6 +119,8 @@ function Display() {
 
     }, [isLoading, data])// to remove the stale closure values
 
+
+    const posts = data?.pages.flat() || [];
     if (isLoading && page === 1) return <p>data is loading please wait....</p>
     if (error) return <p>error occurred</p>
 
@@ -94,7 +141,7 @@ function Display() {
             <div>
 
                 {
-                    allPosts.map((post) => (
+                    posts.map((post) => (
                         <p key={post.id}>{post.id}: {post.title} </p>
                     ))
                 }
