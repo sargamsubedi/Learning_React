282f889dd5b4b590ef2d789232b59d08f776f494
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-31 01:17:49 +0545

    add :basic post method

--- learningPOST/src/App.jsx
+++ learningPOST/src/App.jsx
@@ -1,8 +1,34 @@
 
-
 function App() {
+  const BASE_URL= "https://jsonplaceholder.typicode.com/posts"
+const handleClick = async ()=>{
+  const res = await fetch(
+    BASE_URL,
+    {
+      method: "POST",
+      headers:{
+        "content-type": "application/json"
+      },
+      body:JSON.stringify({
+        name:"sargam",
+        age: 21
+      })
+    }
+  );
+  console.log(res);
+
+  const data = await res.json();
+  console.log(data);
+  
+}
+
   return(
+    <>
     <h1>learning post operations using fetch api</h1>
+    <button onClick={handleClick}>Send data</button>
+
+
+    </>
   )
 }
 
